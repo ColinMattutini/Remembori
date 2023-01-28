09d6bfa2e455b5c056d614930bd5a2a569a64f67
Colin <cmmatt14@gmail.com>
2023-01-28 15:43:13 -0500

    added card iteration for adding cards

--- src/components/FlashCard/FlashCardForm.js
+++ src/components/FlashCard/FlashCardForm.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 const FlashCardForm = () => {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
+  const [cardSet, setCardSet] = useState([]);
+  const [counter, setCounter] = useState(0);
 
   const answerHandler = (e) => {
     setAnswer(e.target.value);
@@ -14,16 +16,39 @@ const FlashCardForm = () => {
   };
 
   const submitCard = () => {
-    console.log(answer, question);
+    setCardSet((cardSet) => [
+      ...cardSet,
+      {
+        question: question,
+        answer: answer,
+      },
+    ]);
+    localStorage.setItem("flashCardSet", JSON.stringify(cardSet));
+    console.log(cardSet);
+  };
+
+  const counterIncreaseHandler = () => {
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+
+  const counterDecreaseHandler = () => {
+    setCounter(counter - 1);
+    console.log(counter);
   };
 
   return (
     <div>
       <h1>Question</h1>
-      <input onChange={questionHandler}></input>
+      <input
+        onChange={questionHandler}
+        value={JSON.stringify(cardSet[counter].question)}
+      ></input>
       <h1>Answer</h1>
       <input onChange={answerHandler}></input>
       <button onClick={submitCard}>Console Log</button>
+      <button onClick={counterDecreaseHandler}>Back</button>
+      <button onClick={counterIncreaseHandler}>Forward</button>
     </div>
   );
 };
