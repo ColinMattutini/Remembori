372ea3ddb08225a820b821739a4aeddacded851d
Colin <cmmatt14@gmail.com>
2023-02-01 17:19:59 -0500

    different states for creating or reviewing cards

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import FlashCard from "./FlashCard/FlashCard";
 import classes from "./Homepage.module.css";
+import ReviewPage from "./Review/ReviewPage";
 
 const Homepage = () => {
+  const [createSet, setCreateSet] = useState(true);
+  const [reviewSet, setReviewSet] = useState(false);
+
+  const createHandler = () => {
+    createSet ? setCreateSet(false) : setCreateSet(true);
+    setReviewSet(false);
+  };
+
+  const reviewHandler = () => {
+    reviewSet ? setReviewSet(false) : setReviewSet(true);
+    setCreateSet(false);
+  };
+
   return (
     <div className={classes.homepage}>
       <div className={classes.alignment}>
-        <FlashCard />
+        <button onClick={createHandler}>Create FlashCard Set</button>
+        <button onClick={reviewHandler}>Review Flashcards</button>
+        {createSet && <FlashCard />}
+        {reviewSet && <ReviewPage />}
       </div>
     </div>
   );
