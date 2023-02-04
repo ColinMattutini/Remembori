1d12ee796550a5420cd8d62fbfa1a217521cba7b
Colin <cmmatt14@gmail.com>
2023-02-04 14:45:11 -0500

    added showing answer or question button

--- src/components/Review/ReviewCard.js
+++ src/components/Review/ReviewCard.js
@@ -5,21 +5,29 @@ const ReviewCard = () => {
     JSON.parse(localStorage.getItem("flashCardSet")),
   ]);
 
+  const [showAnswer, setShowAnswer] = useState(false);
+
   const [cardIndex, setCardIndex] = useState(0);
 
   const testHandler = () => {
     setReviewSet(JSON.parse(localStorage.getItem("flashCardSet")));
   };
 
+  const showAnswerHandler = () => {
+    showAnswer ? setShowAnswer(false) : setShowAnswer(true);
+  };
+
   const cardIndexIncreaser = () => {
     if (cardIndex < reviewSet.length - 1) {
       setCardIndex(cardIndex + 1);
+      setShowAnswer(false);
     }
   };
 
   const cardIndexDecreaser = () => {
     if (cardIndex > 0) {
       setCardIndex(cardIndex - 1);
+      setShowAnswer(false);
     }
   };
 
@@ -32,8 +40,9 @@ const ReviewCard = () => {
     <div>
       <button onClick={testHandler}>Reset</button>
       <h1>Question 1</h1>
-      <h2>{reviewSet[cardIndex].question}</h2>
-      <h2>{reviewSet[cardIndex].answer}</h2>
+      {!showAnswer && <h2>{reviewSet[cardIndex].question}</h2>}
+      {showAnswer && <h2>{reviewSet[cardIndex].answer}</h2>}
+      <button onClick={showAnswerHandler}>Show Answer</button>
       <button onClick={cardIndexDecreaser}>Previous</button>
       <button onClick={cardIndexIncreaser}>Next</button>
     </div>
