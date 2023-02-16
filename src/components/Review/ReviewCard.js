889881d9ea1efabcc2e289037c7ce4f59e686c14
Colin <cmmatt14@gmail.com>
2023-02-16 18:37:00 -0500

    shuffle cards function and shuffle button

--- src/components/Review/ReviewCard.js
+++ src/components/Review/ReviewCard.js
@@ -2,15 +2,17 @@ import React, { useEffect, useState } from "react";
 
 const ReviewCard = () => {
   const [reviewSet, setReviewSet] = useState([
-    JSON.parse(localStorage.getItem("flashCardSet")),
+    JSON.parse(localStorage.getItem("test")),
   ]);
+
+  const [shuffledSet, setShuffledSet] = useState([]);
 
   const [showAnswer, setShowAnswer] = useState(false);
 
   const [cardIndex, setCardIndex] = useState(0);
 
   const testHandler = () => {
-    setReviewSet(JSON.parse(localStorage.getItem("flashCardSet")));
+    setReviewSet(JSON.parse(localStorage.getItem("test")));
   };
 
   const showAnswerHandler = () => {
@@ -31,6 +33,16 @@ const ReviewCard = () => {
     }
   };
 
+  const shuffleCards = (cards) => {
+    for (var i = cards.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var holder = cards[i];
+      cards[i] = cards[j];
+      cards[j] = holder;
+    }
+    console.log(cards);
+  };
+
   useEffect(() => {
     testHandler();
     console.log("Set Added For Review");
@@ -42,6 +54,9 @@ const ReviewCard = () => {
       <h1>Question 1</h1>
       {!showAnswer && <h2>{reviewSet[cardIndex].question}</h2>}
       {showAnswer && <h2>{reviewSet[cardIndex].answer}</h2>}
+      <button onClick={() => setShuffledSet(shuffleCards(reviewSet))}>
+        Shuffle
+      </button>
       <button onClick={showAnswerHandler}>Show Answer</button>
       <button onClick={cardIndexDecreaser}>Previous</button>
       <button onClick={cardIndexIncreaser}>Next</button>
