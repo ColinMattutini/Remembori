5f33d4b90ad72faa5e5ed56a8d0b19c6669887c3
Colin <cmmatt14@gmail.com>
2023-02-19 12:30:24 -0500

    review page added for routing

--- src/components/Review/ReviewPage.js
+++ src/components/Review/ReviewPage.js
@@ -1,8 +1,27 @@
 import React from "react";
 import ReviewCard from "./ReviewCard";
+import ReviewSetSelect from "./ReviewSetSelect";
+import classes from "./ReviewPage.module.css";
+import { useNavigate } from "react-router-dom";
 
 const ReviewPage = () => {
-  return <ReviewCard />;
+  const nav = useNavigate();
+
+  const returnHandler = () => {
+    nav("/home");
+  };
+
+  return (
+    <div className={classes.homepage}>
+      <div className={classes.mainbackground}>
+        <div className={classes.backbutton}>
+          <button onClick={returnHandler}>Back</button>
+        </div>
+        <ReviewCard />
+        {/* <ReviewSetSelect /> */}
+      </div>
+    </div>
+  );
 };
 
 export default ReviewPage;
