e0ee7dc40abb2ab160b0963e4e5b7352f0e8ea1e
Colin <cmmatt14@gmail.com>
2023-02-19 20:06:53 -0500

    individual sets listed on cards

---
+++ src/components/CardSets/IndividualSet.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import classes from "./IndividualSet.module.css";
+
+const IndividualSets = (props) => {
+  const nav = useNavigate();
+  const [name, setName] = useState(Object.keys(localStorage));
+
+  const navHandler = () => {
+    nav("/review/" + props.name);
+  };
+
+  return (
+    <div className={classes.card} onClick={navHandler}>
+      <h1>{props.name}</h1>
+    </div>
+  );
+};
+
+export default IndividualSets;
