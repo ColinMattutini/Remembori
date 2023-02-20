a6240923fd8ec1849a5e939ee9fa163831f8c828
Colin <cmmatt14@gmail.com>
2023-02-19 20:04:05 -0500

    routes added for sets listing and review set cards

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import ReviewPage from "./components/Review/ReviewPage";
 import CreateSetPage from "./Pages/CreateSetPage";
+import ReviewSetsListPage from "./Pages/ReviewSetsListPage";
 
 function App() {
   return (
     <Routes>
-      <Route exact path="/home" element={<Homepage />} />
-      <Route exact path="/review" element={<ReviewPage />} />
+      <Route exact path="/" element={<Homepage />} />
+      <Route exact path="/review/:setName" element={<ReviewPage />} />
+      <Route exact path="/reviewset" element={<ReviewSetsListPage />} />
       <Route exact path="/createset" element={<CreateSetPage />} />
     </Routes>
   );
