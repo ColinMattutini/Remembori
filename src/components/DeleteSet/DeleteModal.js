6ab87f7fc4465288232d31745d39c0529c3535dc
Colin <cmmatt14@gmail.com>
2023-02-25 14:42:01 -0500

    delete function added to remove set from storage

--- src/components/DeleteSet/DeleteModal.js
+++ src/components/DeleteSet/DeleteModal.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Modal from "../UI/Modal";
 import classes from "./DeleteModal.module.css";
 
 const DeleteModal = (props) => {
+  let pathName = window.location.pathname.split("/").pop();
+  pathName = pathName.replaceAll("%20", " ");
+
+  const nav = useNavigate();
+
+  const deleteHandler = () => {
+    localStorage.removeItem(pathName);
+    nav("/reviewset");
+  };
+
   return (
     <Modal>
       <div className={classes.textAlign}>
@@ -13,7 +24,7 @@ const DeleteModal = (props) => {
           <button onClick={props.deleteModalHandler}>Cancel</button>
         </div>
         <div className={classes.deleteButton}>
-          <button>Delete</button>
+          <button onClick={deleteHandler}>Delete</button>
         </div>
       </div>
     </Modal>
