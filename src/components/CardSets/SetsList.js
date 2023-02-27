cb6fb23a67dd6f762aca8518c8ccf167912945a8
Colin <cmmatt14@gmail.com>
2023-02-27 17:30:29 -0500

    added icons for back button on sets page

--- src/components/CardSets/SetsList.js
+++ src/components/CardSets/SetsList.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import IndividualSets from "./IndividualSet";
 import classes from "./SetsList.module.css";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate } from "react-router-dom";
 
 const SetsList = () => {
   const [name, setName] = useState(Object.keys(localStorage));
@@ -9,8 +11,17 @@ const SetsList = () => {
     <IndividualSets name={setNames} />
   ));
 
+  const nav = useNavigate();
+
+  const backButtonHandler = () => {
+    nav("/");
+  };
+
   return (
-    <div>
+    <div className={classes.alignment}>
+      <div className={classes.backButton}>
+        <ArrowBackIcon onClick={backButtonHandler} />
+      </div>
       <div className={classes.cardWrap}>{individualSetCards}</div>
     </div>
   );
