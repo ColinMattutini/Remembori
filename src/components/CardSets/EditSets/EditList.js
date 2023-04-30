3d25d122247503605f4a980ea188ea90abd36f2e
Colin <cmmatt14@gmail.com>
2023-04-30 16:11:46 -0400

    patch req to edit flashcards

--- src/components/CardSets/EditSets/EditList.js
+++ src/components/CardSets/EditSets/EditList.js
@@ -11,8 +11,38 @@ const EditList = () => {
 
   const submitTest = (cards) => {
     cards = cards.filter((item) => item.question);
+    let updateKey = cards[0].setId;
     localStorage.setItem(pathName, JSON.stringify(cards));
+    console.log(updateKey);
+    patchRequest(updateKey, JSON.stringify(cards));
     nav("/review/" + pathName);
+  };
+
+  const patchRequest = async (updateKey, updateValue) => {
+    let token = localStorage.getItem(
+      "CognitoIdentityServiceProvider.5ckk48ttthca3bm3v5dlmapvbi.b29a2bad-578e-45f1-90fb-26e75512103a.idToken"
+    );
+    const response = await fetch(
+      "https://ridrmxlnkl.execute-api.us-east-1.amazonaws.com/Prod/flashcard",
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          setId: updateKey,
+          updateKey: "flashCards",
+          updateValue: updateValue,
+        }),
+        headers: {
+          authToken: token,
+
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    if (response.ok) {
+      console.log("SUCCESS");
+    } else {
+      console.log("FAIL");
+    }
   };
 
   return (
