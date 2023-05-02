6d6079820468d7771674c03ecebe42970568a56d
Colin <cmmatt14@gmail.com>
2023-05-02 18:41:32 -0400

    account status context

---
+++ src/components/Authentication/Account.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { createContext } from "react";
+import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
+import UserPool from "./UserPool";
+
+const AccountContext = createContext();
+
+const Account = (props) => {
+  const getSession = async () => {
+    return await new Promise((resolve, reject) => {
+      const user = UserPool.getCurrentUser();
+      if (user) {
+        user.getSession((err, session) => {
+          if (err) {
+            reject();
+          } else {
+            resolve(session);
+          }
+        });
+      } else {
+      }
+    });
+  };
+
+  const authenticate = async (Username, Password) => {
+    return await new Promise((resolve, reject) => {
+      const user = new CognitoUser({
+        Username,
+        Pool: UserPool,
+      });
+
+      const authDetails = new AuthenticationDetails({
+        Username,
+        Password,
+      });
+
+      user.authenticateUser(authDetails, {
+        onSuccess: (data) => {
+          resolve(data);
+        },
+        onFailure: (err) => {
+          reject(err);
+        },
+        newPasswordRequired: (data) => {
+          resolve(data);
+        },
+      });
+    });
+  };
+
+  const logout = () => {
+    const user = UserPool.getCurrentUser();
+    if (user) {
+      user.signOut();
+      localStorage.clear();
+    }
+  };
+
+  return (
+    <AccountContext.Provider value={{ authenticate, getSession, logout }}>
+      {props.children}
+    </AccountContext.Provider>
+  );
+};
+
+export { Account, AccountContext };
