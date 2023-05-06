f3d0a10237ee6b9ecd9f12922a7a676778945992
Colin <cmmatt14@gmail.com>
2023-05-06 16:13:12 -0400

    status if logged in or not

--- src/components/Authentication/AuthStatus.js
+++ src/components/Authentication/AuthStatus.js
@@ -1,20 +1,28 @@
 import React, { useState, useContext, useEffect } from "react";
 import { AccountContext } from "./Account";
 
-const AuthStatus = () => {
+const AuthStatus = (props) => {
   const [status, setStatus] = useState(false);
   const { getSession, logout } = useContext(AccountContext);
 
+  const newLogout = () => {
+    props.authModalHandler();
+    logout();
+  };
+
   useEffect(() => {
     getSession().then((session) => {
-      console.log("Session: ", session);
+      localStorage.setItem(
+        "Cognitousername",
+        session.idToken.payload["cognito:username"]
+      );
       setStatus(true);
     });
   }, []);
 
   return (
     <div>
-      {status ? <button onClick={logout}>Logout</button> : "Please Login"}
+      {status ? <button onClick={newLogout}>Logout</button> : "Please Login"}
     </div>
   );
 };
