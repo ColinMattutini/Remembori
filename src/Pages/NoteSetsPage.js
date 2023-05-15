4a7ac93bc8e112330baaf62a798df90f8456efa6
Colin <cmmatt14@gmail.com>
2023-05-14 20:00:00 -0400

    notes page for route

---
+++ src/Pages/NoteSetsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NotesSelect from "../components/Notes/NotesListing/NotesSelect";
+
+const NoteSetsPage = () => {
+  return (
+    <div>
+      <NotesSelect />
+    </div>
+  );
+};
+
+export default NoteSetsPage;
